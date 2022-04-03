f7c071e68cad0d9bd3afd8dbf931f60e3f63ad38
Kamil <kjeziorek@student.agh.edu.pl>
2022-04-03 20:39:58 +0200

    initial commit

---
+++ Projekt referencyjny/main.py
@@ -0,0 +1,20 @@
+from gaussian_blur import gaussian_blur
+from canny import sobel_gradient, non_max_suppression, threshold, hysteresis
+from hough import hough_cicrle
+import cv2
+import numpy as np
+
+
+I = cv2.imread("img.jpg")
+I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+
+I_blur = gaussian_blur(I, 5, True)
+mag, angle = sobel_gradient(I_blur, True)
+Z, max_Z = non_max_suppression(mag, angle)
+res = threshold(Z, max_Z)
+output = hysteresis(res)
+img = hough_cicrle(res, 200, 100, 100, 70)
+
+cv2.imshow("Obraz", output.astype('uint8'))
+cv2.imwrite("After Canny.jpg", img.astype('uint8'))
+cv2.waitKey(0)
